eca027d68084bff33fa9b8a9dbef250f7e392d89
HEP CPO Zoey Wu <he8471@nbadmZMacBook-Air.local>
2023-07-23 19:40:04 +0800

    This is will project

--- src/app/article/article-header/article-header.component.ts
+++ src/app/article/article-header/article-header.component.ts
@@ -1,6 +1,14 @@
-
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 import { Article } from '../article.interface';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
@@ -8,17 +16,16 @@ import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-article-header',
   standalone: true,
-  imports: [NgIf,CommonModule ,FormsModule],
+  imports: [NgIf, CommonModule, FormsModule],
   templateUrl: './article-header.component.html',
-  styleUrls: ['./article-header.component.scss']
+  styleUrls: ['./article-header.component.scss'],
 })
 export class ArticleHeaderComponent implements OnChanges {
-
-  @Input() public article:Article = {} as Article;
+  @Input() public article: Article = {} as Article;
   @Output() public delete = new EventEmitter<string>();
   @Output() public titleChanged = new EventEmitter<Article>();
 
-  public IsEdit:boolean = false;
+  public IsEdit: boolean = false;
   orig_item: any;
   itemId: any;
   id!: string;
@@ -26,23 +33,16 @@ export class ArticleHeaderComponent implements OnChanges {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
   }
-  constructor() {
+  constructor() {}
 
+  ngOnChanges({ article }: SimpleChanges) {
+    if (article) {
+      this.orig_item = article.currentValue;
+      //把更改後item的值用object assign給item 變成一個新的值
+      this.article = Object.assign({}, this.orig_item);
+    }
   }
-
-
-
-  ngOnChanges({article}: SimpleChanges) {
-  if(article) {
-  this.orig_item = article.currentValue;
-  //把更改後item的值用object assign給item 變成一個新的值
-  this.article = Object.assign({}, this.orig_item)
-
-  } }
-
-
 
   doEditTitle() {
     this.titleChanged.emit(this.article);
@@ -50,13 +50,12 @@ export class ArticleHeaderComponent implements OnChanges {
     console.log(this.article);
   }
 
-  doCancel(){
-    this.article = Object.assign({},this.orig_item);
-    this.IsEdit =false;
+  doCancel() {
+    this.article = Object.assign({}, this.orig_item);
+    this.IsEdit = false;
   }
 
   doDeleteArticle() {
     this.delete.emit(this.article.id);
   }
-
 }
